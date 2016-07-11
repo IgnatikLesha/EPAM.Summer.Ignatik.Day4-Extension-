4aaf6b2769ce0da5735a7bb4047a27a14837222e
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-11 22:03:25 +0300

    Add project files.

---
+++ ExtensionMethod/ExtensionForDOuble.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class ExtensionForDouble
+    {
+        public static string Performance(this double x)
+        {
+            StringBuilder result = new StringBuilder();
+            byte[] bytes = BitConverter.GetBytes(x);
+
+            foreach (var a in bytes)
+            {
+                result.Insert(0, BinaryPerformance(a));
+            }
+            return result.ToString();
+        }
+
+        private static string BinaryPerformance(int x)
+        {
+            int bits = 8;
+            string result = Convert.ToString(x, 2);
+            int len = result.Length;
+            if (len < bits)
+                for (int i = len; i < bits; i++)
+                    result = result.Insert(0, "0");
+            return result;
+        }
+    }
+}
---
+++ ExtensionMethodTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ExtensionMethod;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace ExtensionMethodTest
+{
+
+    [TestFixture]
+    public class DoubleExtensionTests
+    {
+        [TestCaseSource("Data")]
+        public string Format_Tests(double x)
+        {
+            return ExtensionForDouble.Performance(x);
+        }
+        public IEnumerable<TestCaseData> Data()
+        {
+            yield return new TestCaseData(123.4).Returns("0100000001011110110110011001100110011001100110011001100110011010");
+            yield return new TestCaseData(0.00000001).Returns("0011111001000101011110011000111011100010001100001000110000111010");
+            yield return new TestCaseData(12365625.265554654).Returns("0100000101100111100101011110011100101000011111110110110001111001");
+            yield return new TestCaseData(0.0).Returns("0000000000000000000000000000000000000000000000000000000000000000");
+        }
+    }
+}
